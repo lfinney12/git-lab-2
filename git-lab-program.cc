7ac504dd919bc8d63d2f0d4ad8191bf30643e567
lfinney12 <lf484020@ohio.edu>
2022-01-19 20:12:12 -0500

    git lab program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Enter your name>
+ *        Date: <Enter today's date>
+ * Description: Add Description
+ */
+// Name: Levi Finney 
+// Date: 1/19/21
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
